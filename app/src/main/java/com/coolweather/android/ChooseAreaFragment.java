ab1766ce8d0dbea592da8d1960bfb961b5362bc6
lifefive <nxiaowum@outlook.com>
2020-05-23 12:08:46 +0000

    Add weather update

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -36,7 +36,7 @@ import okhttp3.Response;
 
 public class ChooseAreaFragment extends Fragment {
     /**
-     * 宏定义试图的不同级别
+     * 宏定义试图的不同级别1
      */
     public static final int LEVEL_PROVINCE = 0;
     public static final int LEVEL_CITY = 1;
